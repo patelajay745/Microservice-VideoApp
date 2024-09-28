639106174f1512f29ef30edf0c531161a6b0f116
Ajay patel <patel.ajay745@gmail.com>
2024-09-28 14:37:30 -0400

    setuped projects for playlist microservice

---
+++ playlist/src/config/db.config.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}${DB_NAME}`
+    );
+
+    console.log("mongoDb connected", connectionInstance.connection.name);
+  } catch (error) {
+    console.log("mongoDb connection error", error);
+  }
+};
+
+export default connectDB;
---
+++ playlist/src/utils/cloudinary.js
@@ -0,0 +1,39 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_ACCESS_KEY,
+  api_secret: process.env.CLOUDINARY_ACCESS_TOKEN,
+});
+
+const deleteFilrOnCloudinary = async (cloudinaryImageUrl, fileType) => {
+  try {
+    if (!cloudinaryImageUrl) return null;
+    const publicId = cloudinaryImageUrl.split("/").pop().split(".")[0];
+    const response = await cloudinary.uploader.destroy(publicId, {
+      resource_type: fileType,
+    });
+
+    return response;
+  } catch (error) {
+    console.log("File is not deleted", error);
+  }
+};
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    fs.unlink(localFilePath, () => {});
+    return response;
+  } catch (error) {
+    await fs.unlink(localFilePath, () => {});
+  }
+};
+
+export { uploadOnCloudinary, deleteFilrOnCloudinary };
